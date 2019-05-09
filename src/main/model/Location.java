844c543352d7e78525e5c79e6d6baf7bb041cefc
Amy <amy@Amys-MBP.fuhsd.org>
2019-05-09 08:32:11 -0700

    Add get and set methods for Location

--- src/main/model/Location.java
+++ src/main/model/Location.java
@@ -3,4 +3,15 @@ package main.model;
 public class Location {
 	private double x;
 	private double y;
+	
+	public double getX()
+	{
+		return x;
+	}
+	
+	public double getY()
+	{
+		return y;
+	}
+	
 }
