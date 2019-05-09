f582fe04f15fd4779b5a5bc867af0c81574eaa09
vvictor324 <vvictor324@lhs.win.fuhsd.org>
2019-05-09 08:54:19 -0700

    Change Disaster class

--- src/main/model/Disaster.java
+++ src/main/model/Disaster.java
@@ -3,4 +3,25 @@ package main.model;
 public abstract class Disaster {
 	private String type;
 	private Location loc;
+	
+	public String getType()
+	{
+		return type;
+	}
+	
+	public Location getLoc()
+	{
+		return loc;
+	}
+	
+	public void setType(String newType)
+	{
+		this.type = newType;
+	}
+	
+	
+	public void setLoc(Location newLoc)
+	{
+		this.loc = newLoc;
+	}
 }
