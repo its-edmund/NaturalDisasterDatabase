00b58918dbb4c551e1a06550e5f6d70d29b2faa0
mxinburritos <mxinburritos@gmail.com>
2019-05-10 21:32:52 -0700

    Implement Reverse Geocoding API

--- src/main/model/Location.java
+++ src/main/model/Location.java
@@ -1,6 +1,20 @@
 package main.model;
 
+import eu.bitm.NominatimReverseGeocoding.Address;
+import eu.bitm.NominatimReverseGeocoding.NominatimReverseGeocodingJAPI;
+
 public class Location {
 	private double x;
 	private double y;
+	Address locationAddress;
+
+	public Location(xCoord, yCoord) {
+		NominatimReverseGeocodingJAPI nominatim1 = new NominatimReverseGeocodingJAPI(); // create instance with default
+																						// zoom level (18)
+		
+		locationAddress = nominatim1.getAdress(x, y); //returns Address object for the given position
+	}
+	
+	
+
 }
